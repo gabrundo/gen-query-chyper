b454ede222f31f938cfeda752131c00a2c05eb9a
Gabriele <gabriele.rundo@studenti.unimi.it>
2024-01-29 18:19:09 +0100

    Lettura file JSON

--- src/main/java/uni/tirocinio/generatore/App.java
+++ src/main/java/uni/tirocinio/generatore/App.java
@@ -1,13 +1,36 @@
 package uni.tirocinio.generatore;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.json.*;
+
+public class App {
+    public static void main(String[] args) {
+        Path file = FileSystems.getDefault().getPath("src/main/resources", "1a.json");
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            InputStream in = Files.newInputStream(file);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String line = null;
+
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+
+            JSONObject jObject = new JSONObject(sb.toString());
+            System.out.println(jObject.getJSONObject("sensitive-data"));
+
+        } catch (IOException x) {
+            x.printStackTrace();
+        } catch (JSONException y) {
+            System.err.println("Errroe nella formattazione del file JSON!");
+            y.printStackTrace();
+        }
     }
 }
