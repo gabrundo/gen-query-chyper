d67b8fcdde06bc807eca1edf488e63e678a6c28f
Gabriele <gabriele.rundo@studenti.unimi.it>
2024-01-29 16:18:17 +0100

    Configurazione Maven

---
+++ src/main/java/uni/tirocinio/generatore/App.java
@@ -0,0 +1,13 @@
+package uni.tirocinio.generatore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
